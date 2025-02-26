018871e4c2aca03a493e24d639a4ea2d04e024f5
Daniel Mohr <daniel.mohr@uni-greifswald.de>
2025-02-26 11:55:02 +0100

    moved version info from yaml2script.py to pyproject.toml

--- src/yaml2script/script/yaml2script.py
+++ src/yaml2script/script/yaml2script.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileCopyrightText: 2024-2025 Daniel Mohr
-#    yaml2script.py
-#    Copyright (C) 2024-2025  Daniel Mohr
-#    Version: 0.1.3
-#    License: GPLv3+
 #
-# This script tries to extract the scripts from a '.gitlab-ci.yml' file.
-# In this way the scripts inside '.gitlab-ci.yml' can be tested/analyzed for
-# example with 'shellcheck'.
+# yaml2script extracts the scripts from a '.gitlab-ci.yml' file.
+# Copyright (C) 2024-2025 Daniel Mohr
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 This script extracts the scripts from a '.gitlab-ci.yml' file.
 This allows the scripts inside '.gitlab-ci.yml' to be tested/analyzed
@@ -24,6 +32,10 @@ shellcheck_.gitlab-ci.yml:
     - apk add --no-cache py3-pip py3-yaml shellcheck
     - pip3 install --no-deps --break-system-packages .
     - yaml2script all .gitlab-ci.yml
+
+:Author: Daniel Mohr
+:License: GPLv3+
+:Copyright: (C) 2024-2025 Daniel Mohr
 """
 
 import argparse
